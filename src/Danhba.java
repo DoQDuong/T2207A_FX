0cd0299ea15d1903f316826b96d54927b6aca2e3
DoQDuong <110405690+DoQDuong@users.noreply.github.com>
2023-02-10 16:02:41 +0700

    upbt

--- src/Danhba.java
+++ src/Danhba.java
@@ -1,24 +1,37 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 import javax.xml.soap.Text;
 import java.awt.*;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.ResourceBundle;
 
-public class Danhba {
+public class Danhba implements Initializable {
 
     public Text txtInfor;
     public ObservableList<InDanhBa> listNguoidung = FXCollections.observableArrayList();
-    public ListView<InDanhBa> lv;
-    public InDanhBa editNguoidung;
+//    public ListView<InDanhBa> lv;
+    public static InDanhBa editNguoidung;
     public javafx.scene.text.Text txtInfo;
     public TextField txtName;
     public TextField tlPhone;
+    public static TextField stName;
+    public static TextField stTelephone;
+    public TableColumn<InDanhBa,String> cName;
+    public TableColumn<InDanhBa,String> cTelephone;
+    public TableColumn<InDanhBa,Button> cEdit;
+    public TableView<InDanhBa> tablew;
 
     public void submit(ActionEvent actionEvent) {
         String nn = txtName.getText();
@@ -35,8 +48,8 @@ public class Danhba {
                 }
             }
         }
-        lv.setItems(listNguoidung);
-        lv.refresh();
+        tablew.setItems(listNguoidung);
+        tablew.refresh();
         editNguoidung = null;
         clearInput();
     }
@@ -46,7 +59,7 @@ public class Danhba {
     }
 
     public void edit(MouseEvent mouseEvent) {
-        editNguoidung = lv.getSelectionModel().getSelectedItem();
+//        editNguoidung = lv.getSelectionModel().getSelectedItem();
         txtName.setText(editNguoidung.getTen());
         tlPhone.setText(editNguoidung.getSdt());
     }
@@ -65,5 +78,14 @@ public class Danhba {
             }
         });
     }
-    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cName.setCellValueFactory(new PropertyValueFactory<>("ten"));
+        cTelephone.setCellValueFactory(new PropertyValueFactory<>("sdt"));
+        cEdit.setCellValueFactory(new PropertyValueFactory<>("edit"));
+
+        stName = txtName;
+        stTelephone = tlPhone;
+    }
 }
--- src/InDanhBa.java
+++ src/InDanhBa.java
@@ -1,9 +1,18 @@
+import javafx.scene.control.Button;
+
 public class InDanhBa {
     String ten;
     String sdt;
+    Button Edit;
     public InDanhBa(String ten, String sdt) {
         this.ten = ten;
         this.sdt = sdt;
+        this.Edit = new Button("Edit");
+        this.Edit.setOnAction((event) -> {
+            Danhba.editNguoidung = this;
+            Danhba.stName.setText(this.ten);
+            Danhba.stTelephone.setText(this.sdt);
+        });
     }
 
     public String getTen() {
@@ -24,5 +33,13 @@ public class InDanhBa {
 
     public String toString(){
         return this.ten;
+    }
+
+    public Button getEdit() {
+        return Edit;
+    }
+
+    public void setEdit(Button edit) {
+        Edit = edit;
     }
 }
